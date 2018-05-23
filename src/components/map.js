1c59df01da87b9cdca3411ac368c126213588310
jonalogy <jonalogy@gmail.com>
2018-05-23 22:53:07 +0800

    Centered init map to SG

--- src/components/map.js
+++ src/components/map.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import './map.css';
 import $script from 'scriptjs';
 import { GMAP_KEY } from './gmap-key';
 
+/* eslint-disable no-unused-vars, no-undef */
 // initMap required by Google Maps
 window.initMap = () => {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), { // eslint-disable-line no-undef
-    zoom: 4,
-    center: uluru
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+
+  var singapore = {lat: 1.2977598, lng: 103.7451929};
+  var map = new google.maps.Map(document.getElementById('map'), { 
+    zoom: 10,
+    center: singapore
   });
-  var marker = new google.maps.Marker({ // eslint-disable-line no-undef, no-unused-vars
-    position: uluru,
+  var marker = new google.maps.Marker({ 
+    position: singapore,
     map: map
   });
 };
+/* eslint-enable no-unused-vars, no-undef */
+
 
 const getGmapsUtils = `https://maps.googleapis.com/maps/api/js?key=${GMAP_KEY}&callback=initMap`;
 
@@ -22,7 +28,8 @@ export class Map extends Component {
   componentWillMount() {
     setTimeout(() => console.log("google exists within: ", window.hasOwnProperty('google')), 1000);
     
-    $script.ready('google', () => console.log('Google is ready')); // Alerts when google object is attaced globally.
+    // Alerts when google object is attaced globally.
+    $script.ready('google', () => console.log('Google is ready')); 
     $script(getGmapsUtils, 'google');
   }
 
